2735c742ed21655129a1bb4a5807be54b85f0989
Carolina Morales <carovms@gmail.com>
2021-04-14 10:32:53 -0400

    Firebase iniciado

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const buttonLogin = () => {
+
+    const login = document.getElementById('login');
+    login.addEventListener('click', buttonLogin, false);
+
+}
+
+
+    /*document.getElementById('email').value;
+    document.getElementById('pwd').value;*/
+   
+  
