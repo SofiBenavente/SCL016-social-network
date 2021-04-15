bcaec4b6f95644a44aabf46788af14185c63c2f5
Carolina Morales <carovms@gmail.com>
2021-04-15 09:17:30 -0400

    Login con firebase

--- src/main.js
+++ src/main.js
@@ -5,14 +5,25 @@ import { myFunction } from './lib/index.js';
 myFunction();
 
 const buttonLogin = () => {
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('pwd').value
 
-    const login = document.getElementById('login');
-    login.addEventListener('click', buttonLogin, false);
+    firebase.auth().createUserWithEmailAndPassword(email, pass)
+    .then((userCredential) => {
+        // Signed in
+        let user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+
+    });
 
 }
+const login = document.getElementById('login');
+login.addEventListener('click', buttonLogin, false);
 
 
-    /*document.getElementById('email').value;
-    document.getElementById('pwd').value;*/
-   
-  
