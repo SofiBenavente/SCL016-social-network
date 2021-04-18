7874524e59f6aabb77ef2bb706adee2557079ba2
Carolina Morales <carovms@gmail.com>
2021-04-18 19:36:06 -0400

    login registro y acceso

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import { myFunction } from './lib/index.js';
 
 myFunction();
 
-const buttonLogin = () => {
-    let email = document.getElementById('email').value;
-    let pass = document.getElementById('pwd').value
+const buttonLogin1 = () => {
+    let email = document.getElementById('email1').value;
+    let pass = document.getElementById('pwd1').value
 
+//Función para crear usuarios
     firebase.auth().createUserWithEmailAndPassword(email, pass)
     .then((userCredential) => {
         // Signed in
@@ -23,7 +24,27 @@ const buttonLogin = () => {
     });
 
 }
-const login = document.getElementById('login');
-login.addEventListener('click', buttonLogin, false);
+const login = document.getElementById('login1');
+login.addEventListener('click', buttonLogin1, false);
 
 
+const buttonLogin2 = () => {
+    let email2 = document.getElementById('email2').value;
+    let pass2 = document.getElementById('pwd2').value
+
+//Función para usuarios ya registrados
+    firebase.auth().signInWithEmailAndPassword(email2, pass2)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
+}
+
+const login2 = document.getElementById('login2');
+login2.addEventListener('click', buttonLogin2, false);
