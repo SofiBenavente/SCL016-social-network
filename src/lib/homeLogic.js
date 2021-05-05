a467acb0c623be9a270819d2acbd4f86e8780de2
Carolina Morales <carovms@gmail.com>
2021-05-05 12:43:40 -0400

    cambios en homeView y css

--- src/lib/homeLogic.js
+++ src/lib/homeLogic.js
@@ -1,8 +1,7 @@
-export const homeLogic = async (title, description) => {
+export const homeLogic = async (description) => {
     const db = firebase.firestore();
 
     const response = await db.collection('comments').doc().set({
-        title,
         description
     });
     console.log("response", response);
--- src/lib/homeView.js
+++ src/lib/homeView.js
@@ -4,54 +4,72 @@ export const homeView = () => { /*html*/
   const divHome = document.createElement('div');
   divHome.classList.add('homelobby');
   divHome.innerHTML =
-    `<div class = "homelobby">
+  `<div class = "homelobby">
     <div class="jumbotron" style="margin-bottom:0">
-    <h1>PARASOCIAL</h1>
-    <img src="./img/pngegg.png" id="ghostHome" >
+      <div id="logo">
+        <h1>PARASOCIAL</h1>
+        <img src="./img/pngegg.png" id="ghostHome" >
+      </div>
     </div>
     
-    <section id=navBar>
-      <nav class="navbar navbar-default">
+  <section id=navBar>
+    <nav class="navbar navbar-default">
       <div class="container-fluid">
-      <ul class="nav navbar-nav">
-        <li><a href="#">¿Qué es ParaSocial?</a></li>
-        <li><a href="#">Foros</a></li>
-        <li><a href="#">Tu perfil</a></li>
-    </ul>
-  </div>
-</nav>
-    
-    </section>
+        <ul class="nav navbar-nav">
+          <li><a href="#">¿Qué es ParaSocial?</a></li>
+          <li><a href="#">Foros</a></li>
+          <li><a href="#">Tu perfil</a></li>
+        </ul>
+      </div>
+    </nav>
+  </section>
 
 
   <form id="task-form">
-    <input type="text" id="title" class="form-control" class="Task Title" placeholder="Title" autofocus />
-    
-      <textarea id="description" rows="3" class="form-control" placeholder="Description"></textarea>
-   
-
-    <button class="saveComment" id="saveComment" type="submit">Guardar</button>
-
+    <div id="post">
+      <textarea id="description" rows="3" class="text-area" autofocus placeholder="Description"></textarea>
+      <button class="saveComment" id="saveComment" type="submit">Guardar</button>
+    </div>
+    <div class="hr1"></div>
   </form>
-
+  <button onclick="signOut()"> Cerrar Sesión</button>
   <div class="jumbotron text-center" style="margin-bottom:0">
     <p>Footer</p>
   </div>
-  </div>
-  `
+  </div>`
   
   const taskForm = divHome.querySelector('#task-form');
   taskForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const title = taskForm['title'].value;
     const description = taskForm['description'].value;
-    console.log("titulo y descripcion", title, description);
-    await homeLogic(title, description);
-
-
-
+    console.log("descripcion",description);
+    await homeLogic(description);
+    
   });
 
   return divHome;
 }
+
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ firebase.auth().onAuthStateChanged((user) => {
+   if (user) {
+     if (user.emailVerified === false)
+       document.getElementById('log').innerHTML =
+         `<p>logueado ${user.email} <p>
+           <button onclick="signOut()"> Cerrar Sesión</button>
+           `;
+     console.log(user)
+     // User is signed in, see docs for a list of available properties
+     // https://firebase.google.com/docs/reference/js/firebase.User
+     let uid = user.uid;
+     // ...
+   } else {
+     document.getElementById('log').innerHTML = "No logueado ";
+     // User is signed out
+     // ...
+   }
+ });
+ firebase.analytics();
